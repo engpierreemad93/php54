57f919e6c8493a21fc10c4d014a452b2289c332e
pierreemad93 <pierreemad93@outlook.com>
2021-07-13 20:52:48 +0200

    init project

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*
+   animation :
+   =============
+   syntax 
+   ========
+   $(selector).animate({params},speed,callback);
+*/
+$(document).ready(function(){
+   //start event
+   // $('button').on('click', function(){
+   //    //animate code 
+   //    // $('div').animate({width:'50%',backgroundColor : '#0f0'} , 1000);
+   //    //fade
+   //    $('div').fadeToggle(1000);
+   // });
+   //end event 
+});
